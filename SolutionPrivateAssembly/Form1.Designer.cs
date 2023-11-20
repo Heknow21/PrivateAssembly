01ba18e0e2152a145a3e8e5f7adb938da8784400
manolong.199273 <manolong.199273@LAB-A-PC39>
2023-11-20 18:28:16 +0800

    Add project files.

---
+++ SolutionPrivateAssembly/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace SolutionPrivateAssembly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Compute = new System.Windows.Forms.Button();
+            this.txtBox_number1 = new System.Windows.Forms.TextBox();
+            this.txtBox_number2 = new System.Windows.Forms.TextBox();
+            this.ComboBoxOperation = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // btn_Compute
+            // 
+            this.btn_Compute.Location = new System.Drawing.Point(113, 304);
+            this.btn_Compute.Name = "btn_Compute";
+            this.btn_Compute.Size = new System.Drawing.Size(90, 35);
+            this.btn_Compute.TabIndex = 1;
+            this.btn_Compute.Text = "Compute";
+            this.btn_Compute.UseVisualStyleBackColor = true;
+            this.btn_Compute.Click += new System.EventHandler(this.btn_Compute_Click);
+            // 
+            // txtBox_number1
+            // 
+            this.txtBox_number1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_number1.Location = new System.Drawing.Point(30, 53);
+            this.txtBox_number1.Multiline = true;
+            this.txtBox_number1.Name = "txtBox_number1";
+            this.txtBox_number1.Size = new System.Drawing.Size(259, 40);
+            this.txtBox_number1.TabIndex = 2;
+            this.txtBox_number1.Text = "0";
+            this.txtBox_number1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBox_number2
+            // 
+            this.txtBox_number2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_number2.Location = new System.Drawing.Point(30, 137);
+            this.txtBox_number2.Multiline = true;
+            this.txtBox_number2.Name = "txtBox_number2";
+            this.txtBox_number2.Size = new System.Drawing.Size(259, 45);
+            this.txtBox_number2.TabIndex = 3;
+            this.txtBox_number2.Text = "0";
+            this.txtBox_number2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ComboBoxOperation
+            // 
+            this.ComboBoxOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxOperation.FormattingEnabled = true;
+            this.ComboBoxOperation.Location = new System.Drawing.Point(133, 99);
+            this.ComboBoxOperation.Name = "ComboBoxOperation";
+            this.ComboBoxOperation.Size = new System.Drawing.Size(60, 32);
+            this.ComboBoxOperation.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(30, 194);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(259, 95);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(318, 351);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.ComboBoxOperation);
+            this.Controls.Add(this.txtBox_number2);
+            this.Controls.Add(this.txtBox_number1);
+            this.Controls.Add(this.btn_Compute);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FrmBasicCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Compute;
+        private System.Windows.Forms.TextBox txtBox_number1;
+        private System.Windows.Forms.TextBox txtBox_number2;
+        private System.Windows.Forms.ComboBox ComboBoxOperation;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ SolutionPrivateAssembly/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+
+namespace SolutionPrivateAssembly
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ComboBoxOperation.Items.Add("+");
+            ComboBoxOperation.Items.Add("*");
+            ComboBoxOperation.Items.Add("-");
+            ComboBoxOperation.Items.Add("/");
+        }
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Items.Add("Total: " + Environment.NewLine + "00000"); 
+        }
+        private void btn_Compute_Click(object sender, EventArgs e)
+        {
+            if (txtBox_number1.Text == "" && txtBox_number2.Text == "")
+            {
+                MessageBox.Show("Invalid number input");
+            }
+
+            else if (ComboBoxOperation.Text == "")
+            {
+                MessageBox.Show("Invalid operation input");
+            }
+
+            else
+            {
+               
+                float number1 = (float)Convert.ToDouble(txtBox_number1.Text);
+                float number2 = (float)Convert.ToDouble(txtBox_number2.Text);
+
+
+       
+
+
+
+                if (ComboBoxOperation.Text == "+")
+                {
+                    listView1.Items.Clear();
+                    float computed = BasicCompute.Addition(number1, number2);
+                    string end = computed.ToString();
+                    listView1.Items.Add("Total: " + end);
+                    
+                }
+
+                else if (ComboBoxOperation.Text == "-")
+                {
+                    listView1.Items.Clear();
+                    float computed = BasicCompute.Subtraction(number1, number2);
+                    string end = computed.ToString();
+                    listView1.Items.Add("Total: " + end);
+                    
+                }
+
+                else if (ComboBoxOperation.Text == "*")
+                {
+                    listView1.Items.Clear();
+                    float computed = BasicCompute.Multiplication(number1, number2);
+                    string end = computed.ToString();
+                    listView1.Items.Add("Total: " + end);
+                    
+                }
+
+                else if (ComboBoxOperation.Text == "/")
+                {
+                    listView1.Items.Clear();
+                    float computed = BasicCompute.Division(number1, number2);
+                    string end = computed.ToString();
+                    listView1.Items.Add("Total: " + end);
+                    
+                }
+
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+ 
